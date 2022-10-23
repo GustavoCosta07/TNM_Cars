29e5c8d28a68cb1e93eb725688eaec8b6854dc42
Gustavo Costa <gucostapereira777@gmail.com>
2022-10-23 12:40:25 -0300

    criptografar senha de usuário

--- back_end/controllers/Cadastrar_usuario.js
+++ back_end/controllers/Cadastrar_usuario.js
@@ -1,12 +1,10 @@
 const connection = require('../connection')
+const bcrypt = require('bcrypt')
 
 module.exports = async (request, response) => {
     const data = request.body
 
-    const email = data.email
-    const nome = data.nome
-    const senha = data.senha
-    const confirmacaoDeSenha = data.confirmacaoDeSenha
+    const { email, nome, senha, confirmacaoDeSenha } = data
 
     if (senha.length != 6) {
         return response.status(400).json({ error: 'A senha precisa ter seis caracteres' })
@@ -15,8 +13,10 @@ module.exports = async (request, response) => {
         return response.status(400).json({ error: 'As senhas não conferem' })
     }
 
+    const senhaCryp = await bcrypt.hash(senha, 10)
+
     const query = "INSERT INTO users (email, senha, nome) VALUES (?,?,?)"
-    const insertedUser = await  connection.awaitQuery(query, [email, senha, nome])
+    const insertedUser = await connection.awaitQuery(query, [email, senhaCryp, nome])
 
     response.json(insertedUser)
 
