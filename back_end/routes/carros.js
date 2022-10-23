8d6127d74e625fed25e070c8f364c578110b402b
Gustavo Costa <gucostapereira777@gmail.com>
2022-10-23 17:13:21 -0300

    adicionar middlewares de autenticação

--- back_end/routes/carros.js
+++ back_end/routes/carros.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const carRouter = express.Router();
 const buscarCarroIdeal = require('../controllers/Buscar_carro_ideal')
+const autenticarUsuario = require('../middlewares/Autenticar_usuario')
 
-carRouter.post('/carro_ideal', buscarCarroIdeal)
+carRouter.post('/carro_ideal',autenticarUsuario, buscarCarroIdeal)
+
+
 
 module.exports = carRouter;
