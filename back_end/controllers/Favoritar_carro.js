1cca0882ead339183cb4e85b72e472d6c8ba3b55
Gustavo Costa <gucostapereira777@gmail.com>
2022-10-23 18:45:10 -0300

    adicionar middlewares de autenticação

---
+++ back_end/controllers/Favoritar_carro.js
@@ -0,0 +1,34 @@
+
+const connection = require('../connection')
+
+exports.favoritar = async (request, response) => {
+
+    const { idCarro } = request.body
+
+    if (! idCarro) {
+        return response.status(400).json({ error: 'id do carro não informado' })
+    }
+
+    const query = 'INSERT INTO favoritos (idUser, idCarro) VALUES (?,?)'
+
+    const insertedCarros = await connection.awaitQuery(query, [request.usuario.id, idCarro])
+
+    response.json(insertedCarros)
+
+}
+
+exports.desfavoritar = async (request, response) => {
+
+    const { idCarro } = request.body
+
+    if (! idCarro) {
+        return response.status(400).json({ error: 'id do carro não informado' })
+    }
+
+    const query = `DELETE FROM  favoritos WHERE idUser = ${request.usuario.id} AND idCarro = ${idCarro}`
+    console.log(query)
+
+    const deleteCarros = await connection.awaitQuery(query)
+
+    response.json(deleteCarros)
+}
--- back_end/routes/carros.js
+++ back_end/routes/carros.js
@@ -2,9 +2,13 @@ const express = require('express');
 const carRouter = express.Router();
 const buscarCarroIdeal = require('../controllers/Buscar_carro_ideal')
 const autenticarUsuario = require('../middlewares/Autenticar_usuario')
+const favoritarCarro = require('../controllers/Favoritar_carro')
+const listarCarrosFavoritos = require('../controllers/Listar_carros_favoritos')
 
 carRouter.post('/carro_ideal',autenticarUsuario, buscarCarroIdeal)
-
+carRouter.post('/carro/favoritar',autenticarUsuario,favoritarCarro.favoritar)
+carRouter.post('/carro/desfavoritar',autenticarUsuario,favoritarCarro.desfavoritar)
+carRouter.get('/carro/favoritos',autenticarUsuario,listarCarrosFavoritos)
 
 
 module.exports = carRouter;
